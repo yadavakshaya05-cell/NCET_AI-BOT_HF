93218616a5455c0c4c8b54de3409010434bd9439
yadavakshaya05-cell <yadavakshaya05@gmail.com>
2026-04-15 18:25:14 +0900

    Update APP.py

--- APP.py
+++ APP.py
@@ -13,8 +13,7 @@ st.write("Enter a long text below, and get a concise summary!")
 long_text = st.text_area("Enter text to summarizer:",height=200)
 max_length = st.slider("Max Summary Length",min_value=50,max_value=300,value=150)
 min_length = st.slider("Min Summary Length", min_value=20,max_valse=100,value=30)
-max_length = st.slider("Max Summary Length", min_value=50, max_value=300, val
-min_length = st.slider("Min Summary Length", min_value=20, max_value=100, val
+
 if st.button("Summarize"):
   if long_text.strip():
     with st.spinner("Generating summary... "):
